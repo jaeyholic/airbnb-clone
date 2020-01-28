f19acf1c641a1646ff32d25c9e16fdc15de7aecd
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2020-01-28 23:24:26 +0000

    Reservations model

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Reservations)
+class Reservations(admin.ModelAdmin):
+    """Reservations Admin Definitions"""
+
+    pass
+
