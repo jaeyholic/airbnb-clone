15df00e694c4495fa0bbd4d821c5896503848049
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2020-01-28 23:01:49 +0000

    Rooms and Users Models

---
+++ users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+# Register your models here.
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "bio",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
