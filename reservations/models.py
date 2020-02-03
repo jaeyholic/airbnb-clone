9713651bc061a347050dc235d777b930e721faee
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2020-02-03 12:36:26 +0000

    added reviews to models

--- reservations/models.py
+++ reservations/models.py
@@ -18,8 +18,12 @@ class Reservations(core_models.TimeStampModel):
     status = models.CharField(
         choices=STATUS_CHOICES, max_length=12, default=STATUS_PENDING
     )
-    guests = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    guests = models.ForeignKey(
+        "users.User", related_name="reservations", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reservations", on_delete=models.CASCADE
+    )
     check_in = models.DateField()
     check_out = models.DateField()
 
--- rooms/admin.py
+++ rooms/admin.py
@@ -20,5 +20,52 @@ class PhotoAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic_Info",
+            {"fields": ("name", "description", " country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About The Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "room_type",
+        "city",
+        "facilities",
+        "amenities",
+        "house_rules",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
 
