9ce4de1dc57dbee882565f97b5b5a6032787563b
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2020-02-06 19:45:40 +0000

    rooms and room detail page

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,38 @@
-from math import ceil
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-from django.core.paginator import Paginator
 from . import models
 
 # Create your views here.
-def all_rooms(request):
-    page = request.GET.get("page")
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 15)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"rooms": rooms},)
+class HomeView(ListView):
+    """Homeview Definition"""
+
+    model = models.Room
+    paginate_by = 15
+    ordering = "created"
+
+
+class RoomDetail(DetailView):
+    """RoomDetail Definition"""
+
+    model = models.Room
+
+
+class search(request):
+    pass
+
+
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
+
+
+# def all_rooms(request):
+#     page = request.GET.get("page")
+#     room_list = models.Room.objects.all()
+#     paginator = Paginator(room_list, 15)
+#     rooms = paginator.get_page(page)
+#     return render(request, "rooms/home.html", context={"page": rooms},)
 
